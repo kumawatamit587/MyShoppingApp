8586a341f0394f1ae2eed36faac17015a612351b
Amit Kumar Kumawat <kumawatamit587@gmail.com>
2024-04-21 19:12:12 +0530

    fix

--- Backend/data/items.js
+++ Backend/data/items.js
@@ -1,14 +1,19 @@
-const fs = require('node:fs/promises');
+const fs = require("node:fs/promises");
+const path = require("path");
 
+const jsonFilePath = path.join(__dirname, "..", "items.json");
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  console.log(jsonFilePath);
+  const rawFileContent = await fs.readFile(jsonFilePath, {
+    encoding: "utf-8",
+  });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
 
 function storeItems(items) {
-  return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+  return fs.writeFile(jsonFilePath, JSON.stringify({ items: items || [] }));
 }
 
 exports.getStoredItems = getStoredItems;
